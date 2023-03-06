394c4fc045a8bf138753ceb2a99e3ccd093a457f
Samrat Das <samratdas2602@gmail.com>
2023-03-06 17:52:56 +0530

    Adding Two Directory and some raw code files

---
+++ dir1/Mul.java
@@ -0,0 +1,9 @@
+package dir1;
+
+public class Mul {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println("Answer is " + (a*b));
+    }
+}
---
+++ dir2/Sum.java
@@ -0,0 +1,9 @@
+package dir2;
+
+public class Sum {
+    public static void main(String[] args) {
+        int a = 2;
+        int b = 3;
+        System.out.println("Answer is " + (a+b));
+    }
+}
